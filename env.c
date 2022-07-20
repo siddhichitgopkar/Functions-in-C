b224dcb58b56138fe9d5d61cbb03069049e3404d
Siddhi Chitgopkar <ssc67916@uga.edu>
2022-07-20 15:11:16 -0400

    env code added

--- env.c
+++ env.c
@@ -1,3 +1,9 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
+
+int main(int argc, char **argv, char** env) {
+    while (*env)
+        printf("%s\n", *env++);
+    return 0;
+} //main
